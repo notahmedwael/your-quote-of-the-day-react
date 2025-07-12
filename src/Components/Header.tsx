f0320bac58beee2a54b185ce6f3b6fde43b2e3e9
Ahmed Wael <notahmedwael888@gmail.com>
2025-07-12 17:27:09 +0300

    Changes to responsive design

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -29,26 +29,33 @@ setGenre: (genre: keyof QuoteCollection) => void;
 
 function Header({ genre, setGenre }: HeaderProps) {
 return (
-    <header className="w-full flex flex-row items-center justify-between p-4">
-        <a href=""><h1 className="text-4xl font-mea-culpa font-bold text-center">Your Quote of the Day</h1></a>
-        <div className="flex flex-row items-center gap-4">
+    <header className="w-full flex flex-col md:flex-row items-center justify-between gap-4 p-4">
+        <a href="">
+            <h1 className="text-3xl sm:text-4xl font-mea-culpa font-bold text-center md:text-left">
+            Your Quote of the Day
+            </h1>
+        </a>
+
+        <div className="flex flex-col sm:flex-row items-center gap-2 sm:gap-4">
         <Select value={genre} onValueChange={setGenre}>
-        <SelectTrigger className="w-[180px]">
+            <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="Choose a tone" />
-        </SelectTrigger>
-        <SelectContent>
+            </SelectTrigger>
+            <SelectContent>
             <SelectGroup>
-            <SelectLabel>Tones</SelectLabel>
-            <SelectItem value="Normal">Normal</SelectItem>
-            <SelectItem value="Inspirational">Inspirational</SelectItem>
-            <SelectItem value="Historical">Historical</SelectItem>
+                <SelectLabel>Tones</SelectLabel>
+                <SelectItem value="Normal">Normal</SelectItem>
+                <SelectItem value="Inspirational">Inspirational</SelectItem>
+                <SelectItem value="Historical">Historical</SelectItem>
             </SelectGroup>
-        </SelectContent>
+            </SelectContent>
         </Select>
+
         <ToggleTheme />
     </div>
     </header>
 );
 }
+
 
 export default Header;
